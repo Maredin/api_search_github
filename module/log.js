f9135e5286daa63705f43864f12de63936b3a470
Maredin <94839484+Maredin@users.noreply.github.com>
2023-03-17 18:56:00 +0500

    Update log.js
    log edit

--- module/log.js
+++ module/log.js
@@ -4,6 +4,6 @@ export class LOG {
 
     // Сообщение с числом пользователей
     counterMessage(usersCount) {
-        return (usersCount > 0) ? `Найдено ${usersCount} пользователей` : 'По вашему запросу пользователей не найдено';
+        return (usersCount > 0) ? `Найдено ${usersCount} репозитория по теме` : 'По вашему запросу пользователей не найдено';
     }
 }
